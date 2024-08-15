2ab0a97056d267ee316fa98439f5694692122399
Augusto <augusto.tessaro@pti.org.br>
2024-08-15 17:03:35 -0300

    Add imaginario file

---
+++ Exercicios slide 1/imaginarios.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+
+
+
+
+int main()
+{
+    double z1r, z1i, z2r, z2i, somr, somi, subr, subi;
+
+    printf("Digita os numero real e iimaginario ai cuzao \n");
+    scanf("%lf%lf%lf%lf ", &z1r, &z1i, &z2r, &z2i);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
