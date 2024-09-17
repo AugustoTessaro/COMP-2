f19d2d2ee7114a97ebb20ef8c9b13c6214c63642
Augusto <augusto.tessaro@pti.org.br>
2024-09-17 11:57:35 -0300

    estar exerc 2

--- L2/exerccio_2.c
+++ L2/exerccio_2.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 # include <stdlib.h>
 
+typedef struct Node
+{
+    double data;
+    struct Node *next;
+}Node; 
+
+Node* create_node();
+void append();
+void show_lists(Node *head1, Node *head2);
+
+
 
 int main(){
 
